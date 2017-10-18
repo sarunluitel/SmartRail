0e6710592852776b5a6fd4ad89ad18d021eecf42
Sarun <sarunluitel@gmail.com>
2017-10-17 18:51:22 -0600

    First Commit. Setup packages main, gitignore

---
+++ src/SmartRail/main.java
@@ -0,0 +1,14 @@
+package SmartRail;
+/************************************
+ @author Sarun Luitel
+ ************************************/
+public class main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("smartRail");
+
+  }
+}
+
+
